752faf170488396debe50188022dc22ffbec02fe
jmljayhawk <jmledoux.2017@mymail.becker.edu>
2019-11-16 17:19:41 -0500

    Projectiles
    Projectile scripts moves projctils the proper directions. Prefabs and colliders to prepare for damage.

--- Assets/Scripts/Player2Projectile.cs
+++ Assets/Scripts/Player2Projectile.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player1Projectile : MonoBehaviour
+public class Player2Projectile : MonoBehaviour
 {
     public float speed = 2.0f;
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.right * speed * Time.deltaTime);
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
     }
 }
