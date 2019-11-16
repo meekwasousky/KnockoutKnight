fa96ac1c6cea803e950a819c556bd42c61a6e067
Michael DeProfio <Mikedeprofio@gmail.com>
2019-11-16 12:20:29 -0500

    adding data fro each knight

---
+++ Assets/Scripts/KnghtData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "New Knight Data", menuName = "Knight Data", order = 51)]
+public class KnghtData : ScriptableObject
+{
+
+    [SerializeField]
+    private string knightType;
+    [SerializeField]
+    private string description;
+    [SerializeField]
+    private Sprite icon;
+    [SerializeField]
+    private int heatlth;
+    [SerializeField]
+    private int attackDamage;
+    [SerializeField]
+    private int defense;
+}
