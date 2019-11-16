b3e2f6b7e07c6eec5b6986c27dd3f5a1c855ef79
Michael DeProfio <Mikedeprofio@gmail.com>
2019-11-16 16:56:47 -0500

    character selection

---
+++ Assets/Scripts/Knight.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knight : MonoBehaviour
+{
+    [SerializeField]
+    private GameEvent OnKnightSelected; // 1
+
+    [SerializeField]
+
+    private KnghtData knightData; // 1
+
+    //private void OnMouseDown() // 2
+    //{
+    //    Debug.Log(knightData.KnightType); // 3
+    //    Debug.Log(knightData.Description); // 3
+    //    Debug.Log(knightData.Icon.name); // 3
+    //    Debug.Log(knightData.Health);
+    //    Debug.Log(knightData.Defense); // 3
+    //    Debug.Log(knightData.AttackDamage); // 3
+    //}
+
+    private void OnMouseDown()
+    {
+        OnKnightSelected.Raise(); // 2
+        Debug.Log("event Raised");
+    }
+}
---
+++ Assets/Scripts/KnightStats.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class KnightStats : MonoBehaviour
+{
+    [SerializeField]
+    private Text KnightName; // 1
+    [SerializeField]
+    private Text description; // 2
+    [SerializeField]
+    private Image icon; // 3
+    [SerializeField]
+    private Text health; // 4
+    [SerializeField]
+    private Text attackDamage; // 5
+    [SerializeField]
+    private Text defense; // 5
+
+    public void UpdateDisplayUI(KnghtData knightdata)
+    {
+        KnightName.text = knightdata.KnightType;
+        Debug.Log(knightdata.KnightType);
+        icon.sprite = knightdata.Icon;
+        health.text = knightdata.Health.ToString();
+        attackDamage.text = knightdata.AttackDamage.ToString();
+        defense.text = knightdata.Defense.ToString();
+    }
+}
