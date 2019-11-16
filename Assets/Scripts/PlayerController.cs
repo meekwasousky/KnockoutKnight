a02d7a36c6771115e256d298f1eea89027a5a3ac
jmljayhawk <jmledoux.2017@mymail.becker.edu>
2019-11-16 12:22:23 -0500

    made player controller script
    movement added to player controller.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+     public float speed;
+    public bool animation_bool;
+    public new Animation animation;
+                  
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 1.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerMovement();
+        if (animation_bool == true)
+                     {
+                 animation.Play("Knight_Attack");
+                     }
+                     {
+               animation_bool = true;
+                     }
+    }
+
+    public void playerMovement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.down * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+    }
+}
