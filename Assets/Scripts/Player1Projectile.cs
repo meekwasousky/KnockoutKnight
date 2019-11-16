1f3b1cdb17cfc3aed0d91efe799e7937822371a8
jmljayhawk <jmledoux.2017@mymail.becker.edu>
2019-11-16 17:59:22 -0500

    Projectile Script P1 updated
    Damage values.

--- Assets/Scripts/Player1Projectile.cs
+++ Assets/Scripts/Player1Projectile.cs
@@ -5,10 +5,43 @@ using UnityEngine;
 public class Player1Projectile : MonoBehaviour
 {
     public float speed = 2.0f;
+    
+    public int knightDamage;
+    public int paladinDamage;
+    public int defenderDamage;
+
+    private int projectileDamage;
+
+    void start()
+    {
+        knightDamage = 10;
+        paladinDamage = 8;
+        defenderDamage = 6;
+        if(this.gameObject.tag == "Paladin Projectile")
+        {
+            projectileDamage = paladinDamage;
+        }
+        if(this.gameObject.tag == "Knight Projectile")
+        {
+            projectileDamage = knightDamage;
+        }
+        if(this.gameObject.tag == "Defender Projectile")
+        {
+            projectileDamage = defenderDamage;
+        }
+    }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.right * speed * Time.deltaTime);
+    }
+
+    void OnCollisionEnter2d(Collision2d collision)
+    {
+        if(collision.gameObject.tag == "Knight P2")
+        {
+            
+        }
     }
 }
